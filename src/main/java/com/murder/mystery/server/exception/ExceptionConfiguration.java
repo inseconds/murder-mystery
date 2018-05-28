ad5f8e80e1b965ff3a7b15ea3be4e2c11c34370a
xujunmeng.github <xujunmeng2012@163.com>
2018-05-28 18:13:15 +0800

    init

---
+++ src/main/java/com/murder/mystery/server/exception/ExceptionConfiguration.java
@@ -0,0 +1,21 @@
+package com.murder.mystery.server.exception;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by james on 2018/5/14.
+ */
+@Configuration
+public class ExceptionConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean
+    @ConditionalOnClass(RestExceptionResolver.class)
+    public RestExceptionResolver getRestExceptionResolver() {
+        return new RestExceptionResolver();
+    }
+
+}
---
+++ src/main/java/com/murder/mystery/server/exception/ExceptionResolver.java
@@ -0,0 +1,52 @@
+package com.murder.mystery.server.exception;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.murder.mystery.server.response.DataResponse;
+import com.murder.mystery.server.response.ResponseCode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * com.aihuishou.opt.api.sj
+ * @author Mark Li
+ * @version 1.0.0
+ * @since 2018/4/12
+ */
+public class ExceptionResolver implements HandlerExceptionResolver {
+
+    private static final Logger logger = LoggerFactory.getLogger(ExceptionResolver.class);
+
+	@Override
+    public ModelAndView resolveException(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) {
+
+        DataResponse response;
+        if (e instanceof ServiceException) {
+            logger.warn(e.getMessage());
+            ServiceException se = (ServiceException) e;
+            response =  new DataResponse(se.getResponseCode(), se.getMessage());
+        } else if (e instanceof MethodArgumentNotValidException) {
+            logger.warn(e.getMessage());
+            response = new DataResponse(ResponseCode.BAD_REQUEST, e.getMessage());
+        } else {
+            logger.error(e.getMessage(), e);
+            response = new DataResponse(ResponseCode.SYSTEM_ERROR, e.getMessage());
+        }
+        try {
+        	httpServletResponse.setCharacterEncoding("UTF-8");
+            httpServletResponse.setContentType("application/json");
+            httpServletResponse.getWriter().write(JSON.toJSONString(response, SerializerFeature.WriteMapNullValue));
+        } catch (IOException ioe) {
+            logger.error("与客户端通讯异常：" + ioe.getMessage(), ioe);
+        }
+
+        return new ModelAndView();
+    }
+}
