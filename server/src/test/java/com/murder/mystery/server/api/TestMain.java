eed52bba3cb0e3e47738440e93aa44e32b58aac7
xujunmeng.github <xujunmeng2012@163.com>
2018-06-19 16:50:32 +0800

    init

---
+++ server/src/test/java/com/murder/mystery/server/api/TestMain.java
@@ -0,0 +1,17 @@
+package com.murder.mystery.server.api;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author james
+ * @date 2018/6/14
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath*:applicationContext.xml"})
+public class TestMain {
+
+
+
+}
