fbbb95614034f74619ce35c5f53e34d42d0ae65e
yuehui <yuehui@pinduoduo.com>
2018-07-01 11:41:35 +0800

    mapper

--- sql/DDL.sql
+++ sql/DDL.sql
@@ -1,3 +1,5 @@
+create database murder_mystery default character set utf8mb4 collate utf8mb4_unicode_ci;
+
 CREATE TABLE IF NOT EXISTS `murder_mystery`.`character_evidence` (
   `id` INT(8) AUTO_INCREMENT PRIMARY KEY NOT NULL COMMENT '主键id',
   `drama_id` INT(8) NOT NULL COMMENT '剧本id',
@@ -7,7 +9,7 @@ CREATE TABLE IF NOT EXISTS `murder_mystery`.`character_evidence` (
   `type` INT(4) NULL COMMENT '人物类型',
   `action_cost` INT(4) NULL COMMENT '行动点数')
   COMMENT='人物证据表'
-  ENGINE = INNODB;
+  ENGINE = INNODB DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
   CREATE TABLE IF NOT EXISTS `murder_mystery`.`character` (
   `id` INT(8) AUTO_INCREMENT PRIMARY KEY NOT NULL COMMENT '主键id',
@@ -16,7 +18,7 @@ CREATE TABLE IF NOT EXISTS `murder_mystery`.`character_evidence` (
   `description` VARCHAR(256) NOT NULL COMMENT '角色简介',
   `role_id` INT(4) NULL COMMENT '角色id(侦探,凶手,嫌疑人)')
   COMMENT='人物表'
-  ENGINE = INNODB;
+  ENGINE = INNODB DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
   CREATE TABLE IF NOT EXISTS `murder_mystery`.`drama` (
   `id` INT(8) AUTO_INCREMENT PRIMARY KEY NOT NULL COMMENT '主键id',
@@ -30,7 +32,7 @@ CREATE TABLE IF NOT EXISTS `murder_mystery`.`character_evidence` (
   `paly_count` INT(8) NULL COMMENT '游玩次数',
   `difficulty_degree` INT(4) NULL COMMENT '难度系数')
    COMMENT='剧本表'
-   ENGINE = INNODB;
+   ENGINE = INNODB DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS `murder_mystery`.`user` (
   `id` INT(8) AUTO_INCREMENT PRIMARY KEY NOT NULL COMMENT '主键id',
@@ -39,7 +41,7 @@ CREATE TABLE IF NOT EXISTS `murder_mystery`.`user` (
   `level` INT(4) NULL COMMENT '用户等级',
   `experience` INT(8) NULL COMMENT '用户经验值')
 COMMENT='用户表'
-ENGINE = INNODB;
+ENGINE = INNODB DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS `murder_mystery`.`game_user` (
   `id` INT(8) AUTO_INCREMENT PRIMARY KEY NOT NULL COMMENT '主键id',
@@ -51,7 +53,7 @@ CREATE TABLE IF NOT EXISTS `murder_mystery`.`game_user` (
   `room_id` INT(8) NULL COMMENT '房间id',
   `status` INT(8) NOT NULL COMMENT '用户状态(在线,离线)')
 COMMENT='游戏用户表'
-ENGINE = INNODB;
+ENGINE = INNODB DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS `murder_mystery`.`game_user_evidence` (
   `id` INT(8) AUTO_INCREMENT PRIMARY KEY NOT NULL COMMENT '主键id',
@@ -61,7 +63,7 @@ CREATE TABLE IF NOT EXISTS `murder_mystery`.`game_user_evidence` (
   `checked_user_id` INT(8) NULL COMMENT '证据被查看的用户id',
   `is_open` INT(4) NOT NULL COMMENT '是否公开(0:false 1；true)')
   COMMENT='游戏用户证据表'
-ENGINE = INNODB;
+ENGINE = INNODB DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 
 
@@ -72,6 +74,6 @@ CREATE TABLE IF NOT EXISTS `murder_mystery`.`game_room` (
   `holder_id` INT(8) NULL COMMENT '房主id',
   `status` VARCHAR(45) NULL COMMENT '房间状态(未开始,已开始,进行中,已结束)')
   COMMENT='游戏房间表'
-ENGINE = INNODB;
+ENGINE = INNODB DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
    
